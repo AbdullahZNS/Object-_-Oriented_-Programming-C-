dff196bd40267591dfdd49ca83aa4f786e6d1a45
Abdullah Al Mamun <abdullahmamun.cse20@gmail.com>
2024-12-08 13:26:10 +0600

    Multilevel Inheritance In C++.cpp

--- Multilevel Inheritance In C++.cpp
+++ Multilevel Inheritance In C++.cpp
@@ -1,1 +1,43 @@
 
+// C++ program to implement Multilevel Inheritance
+#include <iostream>
+using namespace std;
+
+//Base class
+class XRP
+{
+ public:
+ void getxrp()
+ {
+ 
+  
+  cout << "xrp will be one of the topest successful cryptocurrency in the world." << endl;
+  
+ }
+};
+// derived class from base class
+class SHIB : public XRP
+{
+ public:
+        void getshib()
+        {
+          cout <<"I will invest in SHIB around 100$." << endl; 
+        }
+};
+// derived from class XRP
+class Cryptocurrency : public SHIB
+{
+ public:
+        void getinvest()
+        {
+          cout << "I will invest around 300$ in cryptocurrency within ten months IN SHA ALLAH." << endl;   
+        }
+};
+int main() {
+  // object of sub class 
+ Cryptocurrency obj;
+ obj.getinvest();
+ obj.getxrp();
+ obj.getshib();
+    return 0;
+}
