e0c5e383e5c9a7a589479017302e78d89810ea17
Abdullah Al Mamun <abdullahmamun.cse20@gmail.com>
2024-12-22 15:42:49 +0600

    Add files via upload

---
+++ Single Inheritance.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+///Base class
+class Vehicle
+{
+  public:
+         Vehicle()
+         {
+          cout << "Starting price of car" << endl;
+         }
+
+};
+
+///Derived class
+class Toyata : public Vehicle
+{
+ public:
+
+
+        Toyota()
+        {
+         cout << "Name : Land Cruiser" << endl;
+         cout << "Price : $57,445" << endl;
+        }
+};
+
+
+int main()
+{
+  Toyota Car;
+  return 0;
+}
